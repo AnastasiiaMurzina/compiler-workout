bc83c87d09d428623a4b7469d2bf524e795880f1
AnastasiiaMurzina <hloshkin@mail.ru>
2018-02-27 14:04:46 +0300

    Update SM.ml
    Again

--- src/SM.ml
+++ src/SM.ml
@@ -33,8 +33,9 @@ let extra_eval conf pr = match conf, pr with
 | _, _ -> failwith "Unexpected instructions" 
 
 let rec eval config prog = match config, prog with
-   | c, [] -> c 
    | c, i::p -> eval (extra_eval c i) p 
+   | c, [] -> c 
+   
 (* Top-level evaluation
      val run : int list -> prg -> int list
    Takes an input stream, a program, and returns an output stream this program calculates
