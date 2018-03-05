4177a91fc56e754be2ae0001ea8c3f21415f4120
AnastasiiaMurzina <hloshkin@mail.ru>
2018-03-05 20:29:12 +0300

    Merge Languge.ml with hw1 and hw2

--- src/Language.ml
+++ src/Language.ml
@@ -44,7 +44,39 @@ module Expr =
        Takes a state and an expression, and returns the value of the expression in 
        the given state.
     *)
-    let eval _ = failwith "Not implemented yet"
+    let to_bin_v v = match v with
+      0 -> false
+    | _ -> true
+
+    let to_int_v v = match v with
+      true -> 1
+    | _ -> 0
+
+    let binop operation v1 v2 = 
+    match operation with
+    | "+" -> v1 + v2
+    | "-" -> v1 - v2
+    | "*" -> v1 * v2
+    | "/" -> v1 / v2
+    | "%" -> v1 mod v2
+    | "<" -> to_int_v (v1 < v2)
+    | "<=" -> to_int_v (v1 <= v2)
+    | ">" -> to_int_v (v1 > v2)
+    | ">=" -> to_int_v (v1 >= v2)
+    | "==" -> to_int_v (v1 = v2)
+    | "!=" -> to_int_v (v1 <> v2)
+    | "&&" -> to_int_v (to_bin_v v1 && to_bin_v v2)
+    | "!!" -> to_int_v (to_bin_v v1 || to_bin_v v2)
+    | _ -> failwith (Printf.sprintf "%s not implemented yet" operation)  
+    
+    let rec eval state expr = 
+    match expr with
+    | Const i -> i
+    | Var x -> state x
+    | Binop (operation, e1, e2) ->
+        let v1 = eval state e1 in
+        let v2 = eval state e2 in
+        binop operation v1 v2
 
     (* Expression parser. You can use the following terminals:
 
@@ -52,6 +84,7 @@ module Expr =
          DECIMAL --- a decimal constant [0-9]+ as a string
    
     *)
+
     ostap (
       parse: empty {failwith "Not implemented yet"}
     )
@@ -78,7 +111,11 @@ module Stmt =
 
        Takes a configuration and a statement, and returns another configuration
     *)
-    let eval _ = failwith "Not implemented yet"
+    let rec eval conf st = match conf, st with
+    | (state, instream, outstream), Assign (x, expr) -> (Expr.update x (Expr.eval state expr) state, instream, outstream)
+    | (state, z::instream, outstream), Read x -> (Expr.update x z state, instream, outstream)
+    | (state, instream, outstream), Write e -> (state, instream, outstream @ [Expr.eval state e])
+    | (state, instream, outstream), Seq (s1, s2) -> eval (eval (state, instream, outstream) s1) s2
 
     (* Statement parser *)
     ostap (
