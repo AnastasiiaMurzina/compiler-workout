323d7ec3cd3793d60d27fec3b01b82fe68feb495
AnastasiiaMurzina <hloshkin@mail.ru>
2018-03-08 17:37:59 +0300

    return two *.mls

--- src/Language.ml
+++ src/Language.ml
@@ -5,7 +5,7 @@ open GT
 
 (* Opening a library for combinator-based syntax analysis *)
 open Ostap.Combinators
-open Ostap
+       
 (* Simple expressions: syntax and semantics *)
 module Expr =
   struct
@@ -38,47 +38,10 @@ module Expr =
     let update x v s = fun y -> if x = y then v else s y
 
     (* Expression evaluator
-
           val eval : state -> t -> int
  
        Takes a state and an expression, and returns the value of the expression in 
        the given state.
-<<<<<<< HEAD
-    *)
-    let to_bin_v v = match v with
-      0 -> false
-    | _ -> true
-
-    let to_int_v v = match v with
-        true -> 1
-    | _ -> 0
-
-    let binop operation v1 v2 = 
-    match operation with
-    | "+" -> v1 + v2
-    | "-" -> v1 - v2
-    | "*" -> v1 * v2
-    | "/" -> v1 / v2
-    | "%" -> v1 mod v2
-    | "<" -> to_int_v (v1 < v2)
-    | "<=" -> to_int_v (v1 <= v2)
-    | ">" -> to_int_v (v1 > v2)
-    | ">=" -> to_int_v (v1 >= v2)
-    | "==" -> to_int_v (v1 = v2)
-    | "!=" -> to_int_v (v1 <> v2)
-    | "&&" -> to_int_v (to_bin_v v1 && to_bin_v v2)
-    | "!!" -> to_int_v (to_bin_v v1 || to_bin_v v2)
-    | _ -> failwith (Printf.sprintf "%s not implemented yet" operation)  
-    
-    let rec eval state expr = 
-    match expr with
-    | Const i -> i
-    | Var x -> state x
-    | Binop (operation, e1, e2) ->
-        let v1 = eval state e1 in
-        let v2 = eval state e2 in
-        binop operation v1 v2
-=======
      *)                                                       
     let to_func op =
       let bti   = function true -> 1 | _ -> 0 in
@@ -105,50 +68,28 @@ module Expr =
       | Const n -> n
       | Var   x -> st x
       | Binop (op, x, y) -> to_func op (eval st x) (eval st y)
->>>>>>> a02d2f10f0fb937b9c20ce1bc554e66439d1508e
 
     (* Expression parser. You can use the following terminals:
-
          IDENT   --- a non-empty identifier a-zA-Z[a-zA-Z0-9_]* as a string
          DECIMAL --- a decimal constant [0-9]+ as a string
                                                                                                                   
     *)
-<<<<<<< HEAD
- let conv_op s = fun x y -> Binop(s, x, y)
- let ostap_bin ops = List.map (fun s -> (ostap ($(s)), conv_op s)) ops
-
-  ostap (
-    primary: x:IDENT {Var x} | x:DECIMAL {Const x} | -"(" parse -")";
-    parse: !(Ostap.Util.expr
-               (fun x -> x)
-               [|
-                `Lefta, ostap_bin ["!!"];
-                 `Lefta, ostap_bin ["&&"];
-                 `Nona,  ostap_bin [">="; ">"; "<="; "<"; "=="; "!="];
-                 `Lefta, ostap_bin ["+"; "-"];
-                 `Lefta, ostap_bin ["*"; "/"; "%"]
-               |]
-              primary
-            )
-  )
-end
-=======
     ostap (                                      
       parse:
-	  !(Ostap.Util.expr 
+    !(Ostap.Util.expr 
              (fun x -> x)
-	     (Array.map (fun (a, s) -> a, 
+       (Array.map (fun (a, s) -> a, 
                            List.map  (fun s -> ostap(- $(s)), (fun x y -> Binop (s, x, y))) s
                         ) 
               [|                
-		`Lefta, ["!!"];
-		`Lefta, ["&&"];
-		`Nona , ["=="; "!="; "<="; "<"; ">="; ">"];
-		`Lefta, ["+" ; "-"];
-		`Lefta, ["*" ; "/"; "%"];
+    `Lefta, ["!!"];
+    `Lefta, ["&&"];
+    `Nona , ["=="; "!="; "<="; "<"; ">="; ">"];
+    `Lefta, ["+" ; "-"];
+    `Lefta, ["*" ; "/"; "%"];
               |] 
-	     )
-	     primary);
+       )
+       primary);
       
       primary:
         n:DECIMAL {Const n}
@@ -157,7 +98,6 @@ end
     )
     
   end
->>>>>>> a02d2f10f0fb937b9c20ce1bc554e66439d1508e
                     
 (* Simple statements: syntax and sematics *)
 module Stmt =
@@ -174,27 +114,9 @@ module Stmt =
     type config = Expr.state * int list * int list 
 
     (* Statement evaluator
-
          val eval : config -> t -> config
-
        Takes a configuration and a statement, and returns another configuration
     *)
-<<<<<<< HEAD
-    let rec eval conf st = match conf, st with
-    | (state, instream, outstream), (Assign (x, expr)) -> (Expr.update x (Expr.eval state expr) state, instream, outstream)
-    | (state, z::instream, outstream), (Read x) -> (Expr.update x z state, instream, outstream)
-    | (state, instream, outstream), (Write e) -> (state, instream, outstream @ [Expr.eval state e])
-    | config, (Seq (s1, s2)) -> eval (eval config s1) s2
-
-    (* Statement parser *)
-    ostap (
-      parse: seq | stmt;
-      stmt: assign | read | write;
-      assign: variable:IDENT -":=" expr:!(Expr.parse) {Assign (variable, expr)};
-      read: "read" -"(" variable:IDENT -")" {Read variable};
-      write: "write" -"(" expr:!(Expr.parse) -")" {Write expr};
-      seq: left_stmt:stmt -";" right_stmt:parse {Seq (left_stmt, right_stmt)}
-=======
     let rec eval ((st, i, o) as conf) stmt =
       match stmt with
       | Read    x       -> (match i with z::i' -> (Expr.update x z st, i', o) | _ -> failwith "Unexpected end of input")
@@ -211,9 +133,7 @@ module Stmt =
         "read" "(" x:IDENT ")"          {Read x}
       | "write" "(" e:!(Expr.parse) ")" {Write e}
       | x:IDENT ":=" e:!(Expr.parse)    {Assign (x, e)}            
->>>>>>> a02d2f10f0fb937b9c20ce1bc554e66439d1508e
     )
-
       
   end
 
@@ -223,13 +143,11 @@ module Stmt =
 type t = Stmt.t    
 
 (* Top-level evaluator
-
      eval : t -> int list -> int list
-
    Takes a program and its input stream, and returns the output stream
 *)
 let eval p i =
   let _, _, o = Stmt.eval (Expr.empty, i, []) p in o
 
 (* Top-level parser *)
-let parse = Stmt.parse                                                     
+let parse = Stmt.parse 
--- src/SM.ml
+++ src/SM.ml
@@ -19,24 +19,9 @@ type prg = insn list
 type config = int list * Stmt.config
 
 (* Stack machine interpreter
-
      val eval : config -> prg -> config
-
    Takes a configuration and a program, and returns a configuration as a result
  *)                         
-<<<<<<< HEAD
-let step_eval config prg = match config, prg with
- | (y::x::st, conf), BINOP p -> ((Expr.binop p x y)::st, conf)
- | (st, conf), CONST z -> (z::st, conf)
- | (st, (state, z::instream, outstream)), READ -> (z::st, (state, instream, outstream))
- | (z::st, (state, instream,outstream)), WRITE -> (st, (state, instream, outstream @ [z]))
- | (st, (state, instream,outstream)), LD x -> ((state x)::st, (state, instream,outstream))
- | (z::st, (state, instream, outstream)), ST x -> (st, (Expr.update x z state, instream, outstream))
- | _ , _ -> failwith "Unexpected"
-
-
- let rec eval = List.fold_left step_eval
-=======
 let rec eval ((stack, ((st, i, o) as c)) as conf) = function
 | [] -> conf
 | insn :: prg' ->
@@ -49,37 +34,17 @@ let rec eval ((stack, ((st, i, o) as c)) as conf) = function
       | LD x     -> (st x :: stack, c)
       | ST x     -> let z::stack'    = stack in (stack', (Expr.update x z st, i, o))
      ) prg'
->>>>>>> a02d2f10f0fb937b9c20ce1bc554e66439d1508e
 
 (* Top-level evaluation
-
      val run : prg -> int list -> int list
-
    Takes a program, an input stream, and returns an output stream this program calculates
 *)
 let run p i = let (_, (_, _, o)) = eval ([], (Expr.empty, i, [])) p in o
 
 (* Stack machine compiler
-
      val compile : Language.Stmt.t -> prg
-
    Takes a program in the source language and returns an equivalent program for the
    stack machine
-<<<<<<< HEAD
- *)
-let rec step_compile = function
- | Expr.Var x -> [LD x]
- | Expr.Const n -> [CONST n]
- | Expr.Binop (op, a, b) -> step_compile a @ step_compile b @ [BINOP op] 
-
-let rec compile  = function
- | Stmt.Assign (x, e) -> step_compile e @ [ST x] 
- | Stmt.Read x -> [READ; ST x]
- | Stmt.Write e -> step_compile e @ [WRITE]
- | Stmt.Seq (s1, s2) -> compile s1 @ compile s2
-
-                         
-=======
 *)
 let rec compile =
   let rec expr = function
@@ -91,5 +56,4 @@ let rec compile =
   | Stmt.Seq (s1, s2)  -> compile s1 @ compile s2
   | Stmt.Read x        -> [READ; ST x]
   | Stmt.Write e       -> expr e @ [WRITE]
-  | Stmt.Assign (x, e) -> expr e @ [ST x]
->>>>>>> a02d2f10f0fb937b9c20ce1bc554e66439d1508e
+| Stmt.Assign (x, e) -> expr e @ [ST x]
